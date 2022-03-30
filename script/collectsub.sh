0095cfdf4dc61d5d6f797bca50c14e82d7802c78
Jane Chen <janechen@cs.utexas.edu>
2022-03-30 02:54:14 -0500

    add mkdir in collectsub script

--- script/collectsub.sh
+++ script/collectsub.sh
@@ -6,12 +6,13 @@ FILENAME=${ARRAY[5]}
 FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
 echo ${NAME}
+mkdir -p /home/janechen/cache/output/$NAME
 
 for f in 2 4 5 7
 do
     for s in 50 100 200 500 1000
     do
-        python ./algs/hierarchy.py -t /home/janechen/cache/traces/feb3-new/$NAME.txt -f ${f} -s ${s} -h 100000 -d 10000000 > /home/janechen/cache/output/$NAME/f${f}-s${s}.txt
+        python ./algs/hierarchy.py -t /home/janechen/cache/traces/feb3/$NAME.txt -f ${f} -s ${s} -h 100000 -d 10000000 > /home/janechen/cache/output/$NAME/f${f}-s${s}.txt
     done
 done
 
