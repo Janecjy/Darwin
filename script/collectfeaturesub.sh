f9a2021a8305e9e8858ab513cb7c83bd83435168
Jane Chen <janechen@cs.utexas.edu>
2022-10-04 01:02:15 -0500

    fix feature output path

--- script/collectfeaturesub.sh
+++ script/collectfeaturesub.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TRACE=$1
+ARRAY=(${TRACE//'/'/ })
 FILENAME=${ARRAY[${#ARRAY[@]}-1]}
 FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
-echo ${NAME}
 
 source venv/bin/activate
 python ./algs/utils/traffic_model/extract_feature.py $TRACE $2$NAME
