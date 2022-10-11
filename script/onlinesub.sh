d8eec495603d3f30098193dd64619a39cec635b5
Jane Chen <janechen@cs.wisc.edu>
2022-10-11 02:04:52 -0500

    change output name in online exp

--- script/onlinesub.sh
+++ script/onlinesub.sh
@@ -8,12 +8,6 @@ ARRAY=(${TRACE//'/'/ })
 FILENAME=${ARRAY[${#ARRAY[@]}-1]}
 FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
-
-TRACE=$1
-ARRAY=(${TRACE//'/'/ })
-FILENAME=${ARRAY[5]}
-FILENAMEARR=(${FILENAME//./ })
-NAME=${FILENAMEARR[0]}
-# echo ${NAME}
+echo ${NAME}
 
 python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/models/ -h 100000 -d 10000000 > $2/$NAME.out
