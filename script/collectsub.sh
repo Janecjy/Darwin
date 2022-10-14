248931fdaf48cd32701a693f658fb84f59c719e5
Jane Chen <janechen@cs.wisc.edu>
2022-10-13 23:27:35 -0500

    collect f7 offline results

--- script/collectsub.sh
+++ script/collectsub.sh
@@ -9,9 +9,9 @@ NAME=${FILENAMEARR[0]}
 # echo ${NAME}
 mkdir -p $2/$NAME
 
-for f in 2 4 5
+for f in 7
 do
-    for s in 50 100 500
+    for s in 50 100 500 1000
     do
         python3 ./algs/hierarchy-static.py -t $TRACE -o $2/$NAME -f ${f} -s ${s} -h 100000 -d 10000000 > $2/$NAME/f${f}-s${s}.txt
     done
