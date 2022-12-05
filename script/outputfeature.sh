b3f8931cbdc74668d6b58b6778e676b1ffdb15f4
Jane Chen <janechen@cs.wisc.edu>
2022-12-05 05:12:05 -0600

    automatically generate path for outputs

--- script/outputfeature.sh
+++ script/outputfeature.sh
@@ -2,11 +2,11 @@
 
 COUNT=0
 FILES=$1'*/'
-mkdir -p /mydata/features-req-window-output
+mkdir -p $TRACE-output
 source venv/bin/activate
 for TRACE in $FILES
 do
-    if [ -f $TRACE"9M.pkl" ]; then
-        python ./script/outputfeature.py $TRACE
+    if [ -f $TRACE"/9M.pkl" ]; then
+        python ./script/outputfeature.py $TRACE/
     fi
 done
