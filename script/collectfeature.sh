75ac7a7c256b75e136380787efcdf55ff814be22
Jane Chen <janechen@cs.wisc.edu>
2022-12-05 22:54:56 -0600

    use 50k for iat, 150k for sd

--- script/collectfeature.sh
+++ script/collectfeature.sh
@@ -4,9 +4,9 @@ COUNT=0
 FILES=$1'*'
 for TRACE in $FILES
 do  
-    for i in 50000 80000 90000 110000 120000 150000
+    for i in 50000 # 50000 80000 90000 110000 120000 150000
     do
-        for s in 50000 80000 90000 100000 110000 120000 150000
+        for s in 150000 # 50000 80000 90000 100000 110000 120000 150000
         do
             mkdir -p $2-$i-$s/
             ./script/collectfeaturesub.sh $TRACE $2-$i-$s/ $i $s &
