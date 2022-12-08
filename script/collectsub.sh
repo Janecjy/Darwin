d7f960669ec999bbd13cf8e6902d6c7e45da74a6
Jane Chen <janechen@cs.wisc.edu>
2022-12-08 09:28:31 -0600

    change size threshold for testing

--- script/collectsub.sh
+++ script/collectsub.sh
@@ -11,7 +11,7 @@ mkdir -p $2$NAME
 
 for f in 2 3 4 5 6 7
 do
-    for s in 50 100 500 1000 5000
+    for s in 10 20
     do
         python3 ./algs/hierarchy-static.py -t $TRACE -o $2$NAME -f ${f} -s ${s} -h 100000 -d 10000000 > $2$NAME/f${f}-s${s}.txt
     done
