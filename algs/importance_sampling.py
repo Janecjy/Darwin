b86a3845f06fec34715019de9842e641bd96633e
Jane Chen <janechen@cs.utexas.edu>
2022-12-12 02:04:53 -0600

    change input to feature+size bucket count; input without normalization

--- algs/importance_sampling.py
+++ algs/importance_sampling.py
@@ -15,12 +15,13 @@ import numpy as np
 
 TRAINING_LEN = 2000000
 PREDICTION_LEN = 2000000
+ROUND_LEN = 500000
 MB = 1000000
-MAX_LIST = [0] * 22
-MIN_LIST = [0] * 22
+MAX_LIST = [0] * 15
+MIN_LIST = [0] * 15
 
 def gen_data(expert_0, expert_1):
-    input = []
+    inputs = []
     labels = []
     prediction_input = []
     prediction_labels = []
@@ -30,21 +31,30 @@ def gen_data(expert_0, expert_1):
     test_files = [path for path in os.listdir("../cache/output/features/test-set-real")]
     # test_index = random.sample(range(num_file), num_file % 100)
     # test_files = ["tc-0-tc-1-2290:0", "tc-0-tc-1-0:24300", "tc-0-tc-1-2290:2916", "tc-0-tc-1-22518:4053", "tc-0-tc-1-2243:488"]
-    feature_set = ['sd_avg', 'iat_avg', 'size_avg', 'edc_avg']
+    # feature_set = ['sd_avg', 'iat_avg', 'size_avg', 'edc_avg']
+    feature_set = ['iat_avg', 'sd_avg', 'size_avg']
     name_list = []
     feature_list = []
     test_set = [] 
+    bucket_list = [10, 20, 50, 100, 500, 100, 5000]
     
     # for i, set in enumerate([train_files, test_files]):
     for i, file in enumerate(train_files+test_files):
+        if i==2:
+            break
         feature = []
         name = file.split('.')[0]
-        name_list.append(name)
+        if not os.path.exists(os.path.join("../cache/output/", expert_0, name+'.pkl')):
+            continue
+        # name_list.append(name)
         if i < len(train_files):
             dir = "train-set"
         else:
             dir = "test-set-real"
             test_set.append(name)
+            
+        print("Collect data from "+dir+" "+name)
+        sys.stdout.flush()
                 
     # for file in ["tc-0-tc-1-2290:0.pkl", "tc-0-tc-1-0:24300.pkl", "tc-0-tc-1-2290:2916.pkl", "tc-0-tc-1-22518:4053.pkl", "tc-0-tc-1-2243:488.pkl"]:
     # file = "tc-0-tc-1-138:958.pkl"
@@ -60,60 +70,71 @@ def gen_data(expert_0, expert_1):
         # print(len(feature))
         feature_list.append(feature)
         
-        assert len(feature) == 22
+        assert len(feature) == 15
 
-        for i, ele in enumerate(feature):
-            if MAX_LIST[i] == 0 or MAX_LIST[i] < ele:
-                MAX_LIST[i] = ele
-            if MIN_LIST[i] == 0 or MIN_LIST[i] > ele:
-                MIN_LIST[i] = ele
+        # for j, ele in enumerate(feature):
+        #     if MAX_LIST[j] == 0 or MAX_LIST[j] < ele:
+        #         MAX_LIST[j] = ele
+        #     if MIN_LIST[j] == 0 or MIN_LIST[j] > ele:
+        #         MIN_LIST[j] = ele
     
-    for i, name in enumerate(name_list):
+    # for i, name in enumerate(name_list):
         
-        if name in test_set:
-            dir = "test-set-real"
-        else:
-            dir = "train-set"
         # print(i)
         # print(feature_list)
         # print(MIN_LIST)
         # print(MAX_LIST)
-        print("Collect data from "+name)
-        sys.stdout.flush()
-        feature = [ (feature_list[i][j]- MIN_LIST[j])/(MAX_LIST[j]-MIN_LIST[j]) for j in range(len(MIN_LIST))]
+        # feature = [ (feature_list[i][j]- MIN_LIST[j])/(MAX_LIST[j]-MIN_LIST[j]) for j in range(len(MIN_LIST))]
+        # feature = [ (feature_list[i][j]- MIN_LIST[j])/(MAX_LIST[j]-MIN_LIST[j]) for j in range(len(MIN_LIST))]
         
-        e0_hits = pickle.load(open(os.path.join("../cache/output/", dir, name, expert_0+'-hits.pkl'), "rb"))
-        e1_hits = pickle.load(open(os.path.join("../cache/output/", dir, name, expert_1+'-hits.pkl'), "rb"))
+        e0_hits = pickle.load(open(os.path.join("../cache/output/", expert_0, name+'.pkl'), "rb"))
+        e1_hits = pickle.load(open(os.path.join("../cache/output/", expert_1, name+'.pkl'), "rb"))
         hit_hit_prob = 0 # pi(e1_hit | e0_hit)
         e0_hit_count = 0
         e0_miss_count = 0
         hit_miss_prob = 0 # pi(e1_hit | e0_miss)
+        bucket_count = [0]*(len(bucket_list)+1)
         
         assert (len(e0_hits) == len(e1_hits))
         
         count = 0
         # import pdb; pdb.set_trace()
         for e0, e1 in zip(e0_hits, e1_hits):
-            if count < TRAINING_LEN:
-                if e0 == 1:
-                    e0_hit_count += 1
-                    if e1 == 1:
-                        hit_hit_prob += 1
-                else:
-                    e0_miss_count += 1
-                    if e1 == 1:
-                        hit_miss_prob += 1
-            else:
-                # if count == TRAINING_LEN:
+            if count > 0 and count % ROUND_LEN == 0:
                 hit_hit_prob = hit_hit_prob/e0_hit_count
                 hit_miss_prob = hit_miss_prob/e0_miss_count
-                if dir == "test-set-real":
-                    prediction_input.append([feature, e0_hit_count, e0_miss_count])
+                input = []
+                input.extend(feature)
+                input.extend(bucket_count)
+                if name in test_set:
+                    prediction_input.append([input, e0_hit_count, e0_miss_count])
                     prediction_labels.append([hit_hit_prob, hit_miss_prob])
                 else:
-                    input.append(feature)
+                    inputs.append(input)
                     labels.append([hit_hit_prob, hit_miss_prob])
-                break
+                e0_hit_count = e0_miss_count = hit_hit_prob = hit_miss_prob = 0
+                bucket_count = [0]*(len(bucket_list)+1)
+            # else:
+                # if count == TRAINING_LEN:
+            if e0[1] == 1:
+                e0_hit_count += 1
+                if e1[1] == 1:
+                    hit_hit_prob += 1
+            else:
+                e0_miss_count += 1
+                if e1[1] == 1:
+                    hit_miss_prob += 1
+                    
+            for j in range(len(bucket_list)):
+                if e1[0] < bucket_list[j]:
+                    bucket_count[j] += 1
+                    # print(count, j)
+                    break
+            if e1[0] >= bucket_list[-1]:
+                bucket_count[-1] += 1
+                # print(count, len(bucket_list))
+            
+                # break
                     # hit_hit_prob = e0_hit_count = e0_miss_count = hit_miss_prob = 0
                 # if name not in test_files:
                 #     break
@@ -132,13 +153,25 @@ def gen_data(expert_0, expert_1):
                 #     if e1 == 1:
                 #         hit_miss_prob += 1
             count += 1
+        if count > 0 and count % ROUND_LEN == 0:
+            hit_hit_prob = hit_hit_prob/e0_hit_count
+            hit_miss_prob = hit_miss_prob/e0_miss_count
+            input = []
+            input.extend(feature)
+            input.extend(bucket_count)
+            if name in test_set:
+                prediction_input.append([input, e0_hit_count, e0_miss_count])
+                prediction_labels.append([hit_hit_prob, hit_miss_prob])
+            else:
+                inputs.append(input)
+                labels.append([hit_hit_prob, hit_miss_prob])
         
     
-    pickle.dump(input, open(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "traininput.pkl"), "wb"))
+    pickle.dump(inputs, open(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "traininput.pkl"), "wb"))
     pickle.dump(labels, open(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "trainlabels.pkl"), "wb"))
     pickle.dump(prediction_input, open(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "predinput.pkl"), "wb"))
     pickle.dump(prediction_labels, open(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "predlabels.pkl"), "wb"))
-    return input, labels, prediction_input, prediction_labels
+    return inputs, labels, prediction_input, prediction_labels
 
 
 def test(device, epoch, model, _data, _label, batch_size):
@@ -146,7 +179,7 @@ def test(device, epoch, model, _data, _label, batch_size):
     # Test the model
     # In the test phase, don't need to compute gradients (for memory efficiency)
     print("=====Epoch {}=====".format(epoch))
-    total_accuracy = 0
+    accuracy = []
     
     # _data = torch.tensor(_data)
     # _label = torch.tensor(_label)
@@ -182,7 +215,11 @@ def test(device, epoch, model, _data, _label, batch_size):
         print("Predicted pi(e1_hit | e0_miss): {}".format(pred_hit_miss_prob))
         print("e1 real hit rate: {}".format(real_e1_hitrate))
         print("e1 predicted hit rate: {}".format(pred_e1_hitrate))
+        accuracy.append((pred_e1_hitrate-real_e1_hitrate)/real_e1_hitrate)
         sys.stdout.flush()
+    mean = sum(accuracy) / len(accuracy)
+    variance = sum([((x - mean) ** 2) for x in accuracy]) / len(accuracy)
+    print("Accuracy avg: {}, var: {}".format(mean, variance))
 
 def main():
     hidden_size = int(sys.argv[1]) # hidden layer node number (2-9)
@@ -194,13 +231,13 @@ def main():
     # expert_1 = "f2s50"
     
     # Check Device configuration
-    if torch.cuda.device_count() < 3:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else:
-        device =  torch.device(device)
+    # if torch.cuda.device_count() < 3:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # else:
+        # device =  torch.device(device)
     
     # # Define Hyper-parameters 
-    input_size = 22
+    input_size = 23
     output_size = 2
     num_epochs = 1000
     # batch_size = 1000
@@ -231,13 +268,16 @@ def main():
         data, label, _data, _label = gen_data(expert_0, expert_1)
     data = torch.tensor(data)
     label = torch.tensor(label)
+    # _data = torch.tensor(_data)
+    # _label = torch.tensor(_label)
     # _data, _label = gen_predictdata(expert_0, expert_1, "tc-0-tc-1-2290:0")
     # Check if model exists
     if not os.path.exists(os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "model-h"+str(hidden_size)+".ckpt")):
         # import pdb; pdb.set_trace()
         
         # data, label = gen_data(expert_0, expert_1)
-        num_data, data_dim = data.shape
+        print(data.shape)
+        [num_data, data_dim] = data.shape
         # _, label_dim = label.shape
         
         num_batch = int(num_data / batch_size)
@@ -282,7 +322,7 @@ def main():
                     sys.stdout.flush()
             
             # Save the model checkpoint
-            # torch.save(model.state_dict(), os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "model-h"+str(hidden_size)+"-"+str(epoch)+".ckpt"))
+            torch.save(model.state_dict(), os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "model-h"+str(hidden_size)+"-"+str(epoch)+".ckpt"))
             test(device, epoch, model, _data, _label, batch_size)
         
         torch.save(model.state_dict(), os.path.join("../cache/output/models/", expert_0+"-"+expert_1, "model-h"+str(hidden_size)+".ckpt"))
