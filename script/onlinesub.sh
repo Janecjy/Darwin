e5bcd1ef3d41c92b7bbd16b9c8cb1c2d3b875bf0
Jane Chen <janechen@cs.wisc.edu>
2023-01-02 03:22:01 -0600

    change model directory

--- script/onlinesub.sh
+++ script/onlinesub.sh
@@ -10,4 +10,4 @@ FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
 echo ${NAME}
 
-python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/models/ -h 100000 -d 10000000 > $2/$NAME-6exp2.out
+python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2/$NAME/online.out
