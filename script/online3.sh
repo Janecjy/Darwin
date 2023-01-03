efa3b23b21f483005112540f3016c40039281ebb
Jane Chen <janechen@cs.wisc.edu>
2023-01-03 00:20:43 -0600

    online3 script background run

--- script/online3.sh
+++ script/online3.sh
@@ -11,7 +11,6 @@ do
     FILENAMEARR=(${FILENAME//./ })
     NAME=${FILENAMEARR[0]}
     echo ${NAME}
-    ./script/onlinesub.sh $TRACE $2 &
-    python3 ./algs/hierarchy-online-3.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2/$NAME/online-3.out
+    python3 ./algs/hierarchy-online-3.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2/$NAME/online-3.out &
 done
 wait
