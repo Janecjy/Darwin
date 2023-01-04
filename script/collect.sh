5539cabd6679aaa48c263af3fd1dca2d5b3685f5
Jane Chen <janechen@cs.wisc.edu>
2023-01-03 22:50:06 -0600

    run expert parallel collection

--- script/collect.sh
+++ script/collect.sh
@@ -5,11 +5,5 @@ FILES=$1'*'
 for TRACE in $FILES
 do
     ./script/collectsub.sh $TRACE $2 &
-    ((COUNT++))
-    if [ $COUNT -eq 30 ]
-        then
-            wait
-            COUNT=0
-    fi
 done
 wait
