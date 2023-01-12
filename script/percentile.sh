89a3a16a99cba1260cf59954f2a2ec043247a6e2
Jane Chen <janechen@cs.wisc.edu>
2023-01-12 01:15:26 -0600

    fix percentile script syntax error

--- script/percentile.sh
+++ script/percentile.sh
@@ -15,6 +15,7 @@ do
     for s in 10 20 30 40 50 60 70 80 90
     do
         for l in 50000 100000 500000 1000000 3000000
+        do
             ((COUNT++))
             python algs/percentile.py  -t $TRACE -o $2$NAME -f ${f} -s ${s} -h 100000 -d 10000000 -l ${l} > $2$NAME/f${f}-s${s}-l${l}.txt &
             if [ $COUNT -eq 30 ]
