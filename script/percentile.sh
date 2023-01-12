005b3f23da4f923c3f6335674c5cdff294277b5f
Jane Chen <janechen@cs.wisc.edu>
2023-01-12 00:50:03 -0600

    add percentile experiment script

---
+++ script/percentile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TRACE=$1'*'
+ARRAY=(${TRACE//'/'/ })
+# echo "${#ARRAY[@]}"
+FILENAME=${ARRAY[${#ARRAY[@]}-1]}
+FILENAMEARR=(${FILENAME//./ })
+NAME=${FILENAMEARR[0]}
+# echo ${NAME}
+mkdir -p $2$NAME
+COUNT=0
+COUNT=0
+for f in 10 20 30 40 50 60 70 80 90
+do
+    for s in 10 20 30 40 50 60 70 80 90
+    do
+        for l in 50000 100000 500000 1000000 3000000
+            ((COUNT++))
+            python algs/percentile.py  -t $TRACE -o $2$NAME -f ${f} -s ${s} -h 100000 -d 10000000 -l ${l} > $2$NAME/f${f}-s${s}-l${l}.txt &
+            if [ $COUNT -eq 30 ]
+            then
+                wait
+                COUNT=0
+            fi
+        done
+    done
+done
