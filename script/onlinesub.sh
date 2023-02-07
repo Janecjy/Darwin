d01a874b48443c418813cf0e0db9555e7a146833
Jane Chen <janechen@cs.wisc.edu>
2023-02-06 22:51:33 -0600

    update onlinesub script output dir

--- script/onlinesub.sh
+++ script/onlinesub.sh
@@ -10,4 +10,4 @@ FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
 echo ${NAME}
 
-python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2/$NAME/online.out
+python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2$NAME/online.out
