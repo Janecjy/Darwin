b4546df7708e3c9ccf6d7796a561c06b1ac79888
Jane Chen <janechen@cs.wisc.edu>
2023-02-06 22:52:49 -0600

    onlinesub create output dir

--- script/onlinesub.sh
+++ script/onlinesub.sh
@@ -8,6 +8,7 @@ ARRAY=(${TRACE//'/'/ })
 FILENAME=${ARRAY[${#ARRAY[@]}-1]}
 FILENAMEARR=(${FILENAME//./ })
 NAME=${FILENAMEARR[0]}
+mkdir -p $2$NAME
 echo ${NAME}
 
 python3 ./algs/hierarchy-online.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2$NAME/online.out
