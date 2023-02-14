398f1f423b9f2de90fe55473775411925a2c7e79
Jane Chen <janechen@cs.utexas.edu>
2023-02-14 07:29:09 -0600

    script to run online for ohr-dw

---
+++ script/online-ohr-dw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COUNT=0
+FILES=$1'*'
+for TRACE in $FILES
+do
+    ARRAY=(${TRACE//'/'/ })
+    # echo "${#ARRAY[@]}"
+    FILENAME=${ARRAY[${#ARRAY[@]}-1]}
+    FILENAMEARR=(${FILENAME//./ })
+    NAME=${FILENAMEARR[0]}
+    mkdir -p $2$NAME
+    echo ${NAME}
+
+    python3 ./algs/hierarchy-online-ohr-dw.py -t $TRACE -m /mydata/ -h 100000 -d 10000000 > $2$NAME/online.out &
+done
+wait
