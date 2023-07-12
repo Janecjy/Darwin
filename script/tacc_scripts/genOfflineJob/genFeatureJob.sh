7263140f2329befc2565731b7695c22d914cec19
Jane Chen <janechen@cs.wisc.edu>
2023-07-12 01:17:56 -0700

    update feature job script

--- script/tacc_scripts/genOfflineJob/genFeatureJob.sh
+++ script/tacc_scripts/genOfflineJob/genFeatureJob.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 BASE_DIR="/scratch1/09498/janechen/mydata/"
-FEATURE_DIR="/scratch1/09498/janechen/mydata/tragen-features/"
 for r in 1 2 5 10
 do
     FILES=$BASE_DIR"tragen-traces-"$r"x/*"
-
+    FEATURE_DIR="/scratch1/09498/janechen/mydata/tragen-features-"$r"x/"
     for TRACE in $FILES
     do
         NAME_EXT=$(basename "$TRACE")  # Extract the filename with extension
@@ -15,7 +14,7 @@ do
         then
             # generate features
             mkdir -p $FEATURE_DIR$NAME
-            echo "./script/collectfeaturesub.sh "$TRACE" "$FEATURE_DIR$NAME" 50000 150000" >> ./script/tacc_scripts/genFeatures/genFeatures_job
+            echo "./script/collectfeaturesub.sh "$TRACE" "$FEATURE_DIR$NAME" 50000 150000" >> ../MultiExpertHOCAdmission-feature/script/tacc_scripts/genFeatures/genFeatures_job
         fi
     done
 done
