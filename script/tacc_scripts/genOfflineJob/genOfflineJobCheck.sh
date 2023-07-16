add016d2f7d17b9848b8fdc46b6f780a06c913c8
Jane Chen <janechen@cs.wisc.edu>
2023-07-16 01:31:54 -0700

    add more job files

--- script/tacc_scripts/genOfflineJob/genOfflineJobCheck.sh
+++ script/tacc_scripts/genOfflineJob/genOfflineJobCheck.sh
@@ -29,10 +29,10 @@ do
                             if [[ -e $OUTPUT_DIR$NAME/$EXPERT0-hits.txt ]] && [[ -e $OUTPUT_DIR$NAME/$EXPERT1-hits.txt ]] # && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT0-hits.txt) -eq 99000000 ]] && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT1-hits.txt) -eq 99000000 ]]
                             then
                                 # generate correlation with data
-                                echo "python3 algs/correlation_data_gen_w_size.py "${EXPERT0}" "${EXPERT1}" "${TRACE}" "$BASE_DIR" "$OUTPUT_DIR" "$CORR_DIR >> ./script/tacc_scripts/genCorr/genCorrWData_job
+                                echo "python3 algs/correlation_data_gen_w_size.py "${EXPERT0}" "${EXPERT1}" "${TRACE}" "$BASE_DIR" "$OUTPUT_DIR" "$CORR_DIR >> "./script/tacc_scripts/genCorr/genCorrWData_"$r"x_job"
                             else
                                 # generate correlation without data
-                                echo "python3 algs/correlation_gen_per_pair.py -t "${TRACE}" -i "${FEATURE_DIR}" -c "${CORR_DIR}" -f "${f0}" -s "${s0}" -g "${f1}" -u "${s1}" -h "$((100000 * r))" -d "$((10000000 * r)) >> ./script/tacc_scripts/genCorr/genCorrWoData_job
+                                echo "python3 algs/correlation_gen_per_pair.py -t "${TRACE}" -i "${FEATURE_DIR}" -c "${CORR_DIR}" -f "${f0}" -s "${s0}" -g "${f1}" -u "${s1}" -h "$((100000 * r))" -d "$((10000000 * r)) >> "./script/tacc_scripts/genCorr/genCorrWoData_"$r"x_job"
                             fi
                         fi
                     done
