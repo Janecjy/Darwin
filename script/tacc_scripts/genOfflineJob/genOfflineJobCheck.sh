d81fb344abf8114878e3a0213fc42894854177e3
Jane Chen <janechen@cs.wisc.edu>
2023-07-17 06:21:07 -0700

    check remaining jobs

--- script/tacc_scripts/genOfflineJob/genOfflineJobCheck.sh
+++ script/tacc_scripts/genOfflineJob/genOfflineJobCheck.sh
@@ -18,24 +18,28 @@ do
                 then
                     EXPERT0=f${f0}s${s0}
                     EXPERT1=f${f1}s${s1}
-                    mkdir -p $CORR_DIR${EXPERT0}-${EXPERT1}
-                    for TRACE in $FILES
-                    do
-                        NAME_EXT=$(basename "$TRACE")  # Extract the filename with extension
-                        NAME=${NAME_EXT%.*}
-                        regex='.*-[0-6]$'
-                        if [[ "$NAME" =~ $regex ]] 
-                        then
-                            if [[ -e $OUTPUT_DIR$NAME/$EXPERT0-hits.txt ]] && [[ -e $OUTPUT_DIR$NAME/$EXPERT1-hits.txt ]] # && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT0-hits.txt) -eq 99000000 ]] && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT1-hits.txt) -eq 99000000 ]]
+                    # if directory not empty
+                    if [ ! "$(ls -A $CORR_DIR${EXPERT0}-${EXPERT1})" ]
+                    then
+                        mkdir -p $CORR_DIR${EXPERT0}-${EXPERT1}
+                        for TRACE in $FILES
+                        do
+                            NAME_EXT=$(basename "$TRACE")  # Extract the filename with extension
+                            NAME=${NAME_EXT%.*}
+                            regex='.*-[0-6]$'
+                            if [[ "$NAME" =~ $regex ]] 
                             then
+                                # if [[ -e $OUTPUT_DIR$NAME/$EXPERT0-hits.txt ]] && [[ -e $OUTPUT_DIR$NAME/$EXPERT1-hits.txt ]] # && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT0-hits.txt) -eq 99000000 ]] && [[ $(wc -l < $OUTPUT_DIR$NAME/$EXPERT1-hits.txt) -eq 99000000 ]]
+                                # then
                                 # generate correlation with data
                                 echo "python3 algs/correlation_data_gen_w_size.py "${EXPERT0}" "${EXPERT1}" "${TRACE}" "$BASE_DIR" "$OUTPUT_DIR" "$CORR_DIR >> "./script/tacc_scripts/genCorr/genCorrWData_"$r"x_job"
-                            else
-                                # generate correlation without data
-                                echo "python3 algs/correlation_gen_per_pair.py -t "${TRACE}" -i "${FEATURE_DIR}" -c "${CORR_DIR}" -f "${f0}" -s "${s0}" -g "${f1}" -u "${s1}" -h "$((100000 * r))" -d "$((10000000 * r)) >> "./script/tacc_scripts/genCorr/genCorrWoData_"$r"x_job"
+                                # else
+                                #     # generate correlation without data
+                                #     echo "python3 algs/correlation_gen_per_pair.py -t "${TRACE}" -i "${FEATURE_DIR}" -c "${CORR_DIR}" -f "${f0}" -s "${s0}" -g "${f1}" -u "${s1}" -h "$((100000 * r))" -d "$((10000000 * r)) >> "./script/tacc_scripts/genCorr/genCorrWoData_"$r"x_job"
+                                # fi
                             fi
-                        fi
-                    done
+                        done
+                    fi
                 fi
             done
         done
