c90dbe93c5a5bc0c66da97e6a79c27bc593af721
Jane Chen <janechen@cs.wisc.edu>
2023-07-21 00:13:39 -0700

    add train scripts

--- script/tacc_scripts/genTrainJob/genTrainJob.sh
+++ script/tacc_scripts/genTrainJob/genTrainJob.sh
@@ -2,7 +2,7 @@
 
 r=$1
 HIDDEN=2
-BASE_DIR="/scratch1/09498/janechen/mydata/"
+BASE_DIR="/scratch2/09498/janechen/"
 for f0 in 2 3 4 5 6 7
 do
     for s0 in $((10 * r)) $((20 * r)) $((50 * r)) $((100 * r)) $((500 * r)) $((1000 * r))
@@ -18,7 +18,7 @@ do
                     CORR_DIR=$BASE_DIR"tragen-correlations-"$r"x/"${EXPERT0}-${EXPERT1}
                     MODEL_DIR=$BASE_DIR"tragen-models-"$r"x/"${EXPERT0}-${EXPERT1}
                     mkdir -p $MODEL_DIR
-                    echo "python3 algs/train.py "$HIDDEN" "${EXPERT0}" "${EXPERT1}" "$CORR_DIR" "$MODEL_DIR" > "$MODEL_DIR"/"$HIDDEN"-result.out"
+                    echo "python3 algs/train.py "$HIDDEN" "${EXPERT0}" "${EXPERT1}" "$CORR_DIR" "$MODEL_DIR" > "$MODEL_DIR"/"$HIDDEN"-result.out" > "./script/tacc_scripts/train/train_"$r"x_job"
             done
         done
     done
