9ebca2f35a67da408e56429e18dd9f58f0a2d4d4
Jane Chen <janechen@cs.wisc.edu>
2023-07-25 09:02:12 -0700

    add scripts to draw enemble

---
+++ script/eval-data/draw-ensemble-ohr-5x.py
@@ -0,0 +1,82 @@
+import random
+import pickle
+import os
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sns.set(font_scale=3, style='white')
+rate = 5
+BASE_DIR = "/scratch2/09498/janechen/"
+online_result = pickle.load(open(BASE_DIR+"tragen-output-online-"+str(rate)+"x", "rb"))
+
+trace_list = list(online_result.keys())
+# trace_list = ['tc-0-1-150:115', 'tc-0-1-136:128', 'tc-0-1-80:185', 'tc-0-1-244:21', 'tc-0-1-2:263', 'tc-0-1-206:59', 'tc-0-1-0:265', 'tc-0-1-265:0', 'tc-0-1-8:257']
+    
+baseline_list = []
+# for f in [2, 3, 4, 5, 6, 7]:
+# # for f in [2, 3, 4]:
+#     # for s in [20, 500]:
+#     for s in [10, 20, 50, 100, 500, 1000]:
+#         baseline_list.append('f'+str(f)+'s'+str(s*rate))
+baseline_list.append("tragen-output-percentile-"+str(rate)+"x")
+# baseline_list.append("hillclimbing")
+baseline_list.append("tragen-output-hillclimbing-c10-"+str(rate)+"x")
+baseline_list.append("tragen-output-hillclimbing-c20-"+str(rate)+"x")
+# baseline_list.append("hillclimbing-continuous-c20")
+# baseline_list.append("hillclimbing-continuous-c50")
+# baseline_list.append("hillclimbing-continuous-c100")
+
+diff_data = []
+diff_percentage_data = []
+for baseline in baseline_list:
+    baseline_result = pickle.load(open(BASE_DIR+baseline+"/results.pkl", "rb"))
+    # print(baseline)
+    diff = []
+    diff_percentage = []
+    for trace in trace_list:
+        if trace not in baseline_result.keys() or not baseline_result[trace]:
+            # print(trace, baseline)
+            continue
+        
+        diff.append(online_result[trace]-baseline_result[trace])
+        diff_percentage.append((online_result[trace]-baseline_result[trace])/online_result[trace]*100)
+        # baseline_data.append(result[trace]['6exp-online']-result[trace][baseline])
+    diff_data.append(diff)
+    diff_percentage_data.append(diff_percentage)
+    print("for baseline {}, the avg improvement is {}, the avg improvement rate is {}".format(baseline, sum(diff)/len(diff), sum(diff_percentage)/len(diff_percentage)))
+
+# pickle.dump(diff_data, open("./diff_data.pkl", "wb"))    
+# pickle.dump(diff_percentage_data, open("./diff_percentage_data.pkl", "wb"))   
+
+sns.set_palette(palette=sns.color_palette("deep"))
+# diff_data = pickle.load(open("./diff_data.pkl", "rb"))
+# diff_percentage_data = pickle.load(open("./diff_percentage_data.pkl", "rb"))
+diff_data_dict = {}
+diff_percentage_data_dict = {}
+for i, baseline in enumerate(baseline_list):
+    if baseline == "tragen-output-percentile-"+str(rate)+"x":
+        baseline = "P"
+    if baseline == "tragen-output-hillclimbing-c10-"+str(rate)+"x":
+        baseline = "HC-s10"
+    if baseline == "tragen-output-hillclimbing-c10-"+str(rate)+"x":
+        baseline = "HC-s20"
+    diff_data_dict[baseline] = diff_data[i]
+    diff_percentage_data_dict[baseline] = diff_percentage_data[i]
+# print(len(diff_percentage_data_dict["P"]))
+# diff_percentage_data_dict["AS"] = [-4, -1.3, -.1, 3, 12.4, 68.2, .7, 5, -.7]
+# print("for baseline {}, the avg improvement is {}, the avg improvement rate is {}".format("adaptsize", sum(diff)/len(diff), sum(diff_percentage_data_dict["AS"])/len(diff_percentage_data_dict["AS"])))
+# diff_percentage_data_dict["DM"] = [-.9, -1.2, 1.5, 1.2, 11, 27, 7, .1, .7]
+# print("for baseline {}, the avg improvement is {}, the avg improvement rate is {}".format("directmapping", sum(diff)/len(diff), sum(diff_percentage_data_dict["DM"])/len(diff_percentage_data_dict["DM"])))
+    
+df_diff = pd.DataFrame.from_dict(diff_data_dict)
+df_diff_percentage = pd.DataFrame.from_dict(diff_percentage_data_dict)
+
+
+plt.figure(figsize=(15, 10))
+ax = sns.boxplot(data=df_diff_percentage, color="C10")
+ax.set_xticklabels(ax.get_xticklabels(),rotation=70)
+ax.axhline(0, color="C3")
+plt.ylabel("HOC OHR Improvement Rate (%)")
+# ax = sns.lineplot(x=[0]*len(baseline_list), y=[y*5 for y in range(len(baseline_list))], ax=ax, linewidth = 2)
+plt.savefig("baseline-improvement-percentage.png",bbox_inches='tight')
