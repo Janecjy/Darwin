ae567ef7f22055fe2d4199dfcd1126d6a4184a60
Jane Chen <janechen@cs.wisc.edu>
2023-07-27 10:58:44 -0700

    check deployed experts of bandit rounds

--- script/eval-data/bandit_rounds.py
+++ script/eval-data/bandit_rounds.py
@@ -8,7 +8,7 @@ import seaborn as sns
 sns.set(font_scale=1.5, style='white')
 
 dir_path = sys.argv[1]
-converge_times = 10
+converge_times = 5
 round_length = []
 for f in os.listdir(dir_path):
     if not f.endswith(".txt"):
@@ -23,12 +23,15 @@ for f in os.listdir(dir_path):
             start = True
         if start and line.startswith("['f"):
             potential_length = len(line.split(','))
-        if start and not finish and line.startswith("{'f"):
-            values = []
-            for l in line.split(','):
-                values.append(float(l.split(':')[1].replace("}\n", "")))
-            # print(values)
-            history.append(np.argmax(values))
+        if start and not finish and line.startswith("Round"):
+            exp = line.split(',')[1].split("is")[1].replace(" ", "")
+            history.append(exp)
+        # if start and not finish and line.startswith("{'f"):
+        #     values = []
+        #     for l in line.split(','):
+        #         values.append(float(l.split(':')[1].replace("}\n", "")))
+        #     # print(values)
+        #     history.append(np.argmax(values))
         if len(history) > converge_times:
             converge = True
             for j in range(converge_times):
